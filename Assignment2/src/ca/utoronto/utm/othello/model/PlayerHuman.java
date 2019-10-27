16c3ebdd94b07c40012cc02fcd77aee871528d47
Jiaqi Chen <isabeljiaqi.chen@mail.utoronto.ca>
2019-10-27 11:43:18 -0400

    a little modify about user story 1.011

--- Assignment2/src/ca/utoronto/utm/othello/model/PlayerHuman.java
+++ Assignment2/src/ca/utoronto/utm/othello/model/PlayerHuman.java
@@ -29,33 +29,33 @@ public class PlayerHuman extends Player {
 ////		int col = getMove("col: ");
 //		return move;
 //	}
-//	public Move getMove() {
-//		int row = getMove("row: ");
-//		int col = getMove("col: ");
-//		return new Move(row, col);
-//	}
-//
-//	private int getMove(String message) {
-//		int move, lower = 0, upper = 7;
-//		while (true) {
-//			try {
-//				System.out.print(message);
-//				String line = PlayerHuman.stdin.readLine();
-//				move = Integer.parseInt(line);
-//				if (lower <= move && move <= upper) {
-//					return move;
-//				} else {
-//					System.out.println(INVALID_INPUT_MESSAGE);
-//				}
-//			} catch (IOException e) {
-//				System.out.println(INVALID_INPUT_MESSAGE);
-//				break;
-//			} catch (NumberFormatException e) {
-//				System.out.println(INVALID_INPUT_MESSAGE);
-//			}
-//		}
-//		return -1;
-//	}
+	public Move getMove() {
+		int row = getMove("row: ");
+		int col = getMove("col: ");
+		return new Move(row, col);
+	}
+
+	private int getMove(String message) {
+		int move, lower = 0, upper = 7;
+		while (true) {
+			try {
+				System.out.print(message);
+				String line = PlayerHuman.stdin.readLine();
+				move = Integer.parseInt(line);
+				if (lower <= move && move <= upper) {
+					return move;
+				} else {
+					System.out.println(INVALID_INPUT_MESSAGE);
+				}
+			} catch (IOException e) {
+				System.out.println(INVALID_INPUT_MESSAGE);
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println(INVALID_INPUT_MESSAGE);
+			}
+		}
+		return -1;
+	}
 }
 
 
