815aa34416cafe4d6e1f6401e889404fbbcbd6a1
Osama Saleh <osama.saleh@mail.utoronto.ca>
2019-11-11 23:21:56 -0500

    Scrum Meeting and working on US1.13

--- Assignment2/src/ca/utoronto/utm/othello/viewcontroller/BoardSquare.java
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/BoardSquare.java
@@ -4,14 +4,19 @@ import ca.utoronto.utm.othello.model.Othello;
 import ca.utoronto.utm.util.Observable;
 import ca.utoronto.utm.util.Observer;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class BoardSquare extends Button implements Observer{
 	private Othello othello;
+	private ImageView currImage;
+	int count = 0;
 	
 	public BoardSquare(Othello othello, int row, int col) {
 		this.setText("");
 		this.setGraphic(othello.getImage(row,col));
+		currImage = othello.getImage(row, col);
+		
 	}
 	
 	@Override
@@ -20,6 +25,16 @@ public class BoardSquare extends Button implements Observer{
 		int row = GridPane.getRowIndex(this);
 		int col = GridPane.getColumnIndex(this);
 		this.setGraphic(this.othello.getImage(row,col));
+		if (othello.getImage(row, col) != currImage) {
+			//Animate
+			System.out.println(row + " " + col);
+			this.setStyle("-fx-background-color: MediumSeaGreen");
+			currImage = this.othello.getImage(row, col);
+			
+			
+		}
+		
+		
 	}
 
 }
