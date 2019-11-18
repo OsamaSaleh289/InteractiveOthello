d0bfe579dd48f5e8824c93d1521c37d55983081b
dominikwrona <dominikwrona@192.168.1.5>
2019-11-17 22:25:08 -0500

    Resolved merge conflicts

--- Assignment2/src/ca/utoronto/utm/othello/viewcontroller/RestartEventHandler.java
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/RestartEventHandler.java
@@ -23,29 +23,3 @@ public class RestartEventHandler implements EventHandler<ActionEvent> {
 	}
 
 }
-=======
-package ca.utoronto.utm.othello.viewcontroller;
-
-import ca.utoronto.utm.othello.model.Othello;
-import ca.utoronto.utm.othello.model.TimeTracker;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-
-public class RestartEventHandler implements EventHandler<ActionEvent> {
-	private Othello othello;
-	private TimeTracker timer;
-	
-	public RestartEventHandler(Othello othello, TimeTracker timer) {
-		this.othello = othello;
-		this.timer = timer;	
-	}
-
-	@Override
-	public void handle(ActionEvent event) {
-		TimeQuery query = new TimeQuery();
-		this.othello.resetOthello();
-		query.run();
-		this.timer.startTimer();
-	}
-
-}
--- Assignment2/src/ca/utoronto/utm/othello/viewcontroller/TimeQuery.java
+++ Assignment2/src/ca/utoronto/utm/othello/viewcontroller/TimeQuery.java
@@ -1,3 +1,4 @@
+
 package ca.utoronto.utm.othello.viewcontroller;
 
 import java.util.Optional;
@@ -6,9 +7,6 @@ import ca.utoronto.utm.othello.model.TimeTrackerSingleton;
 import javafx.scene.control.TextInputDialog;
 
 public class TimeQuery {
-	/**
-	 * creates two dialogs that allow the user to set time
-	 */
 	public void run() {
 		TextInputDialog dialogp1 = new TextInputDialog();
 		dialogp1.setTitle("");
@@ -28,36 +26,6 @@ public class TimeQuery {
 		});
 		TimeTrackerSingleton.setSecondsP1(0);
 		TimeTrackerSingleton.setSecondsP2(0);
-	}
-}
-=======
-package ca.utoronto.utm.othello.viewcontroller;
-
-import java.util.Optional;
-
-import ca.utoronto.utm.othello.model.TimeTracker;
-import javafx.scene.control.TextInputDialog;
-
-public class TimeQuery {
-	public void run() {
-		TextInputDialog dialogp1 = new TextInputDialog();
-		dialogp1.setTitle("");
-		dialogp1.setHeaderText("Please enter the time (in minutes) for Player 1");
-		dialogp1.setGraphic(null);
-		Optional<String> result = dialogp1.showAndWait();
-		result.ifPresent(time -> {try {int p1time = Integer.parseInt(time);TimeTracker.setMinutesP1(p1time);} catch(Exception e) {
-			System.out.println("invalid, time must be an integer");};
-		});
-		TextInputDialog dialogp2 = new TextInputDialog();
-		dialogp2.setTitle("");
-		dialogp2.setHeaderText("Please enter the time (in minutes) for Player 2");
-		dialogp2.setGraphic(null);
-		Optional<String> result2 = dialogp2.showAndWait();
-		result2.ifPresent(time -> {try {int p2time = Integer.parseInt(time);TimeTracker.setMinutesP2(p2time);} catch(Exception e) {
-			System.out.println("invalid, time must be an integer");}
-		});
-		TimeTracker.setSecondsP1(0);
-		TimeTracker.setSecondsP2(0);
 	}
 }
 
